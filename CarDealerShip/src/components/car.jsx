b18e0f9474fb79943276a33bd964d499b59cee18
ViceElez <vice.elezovic12@gmail.com>
2025-02-18 16:48:01 +0100

    add car component and data

---
+++ CarDealerShip/src/components/car.jsx
@@ -0,0 +1,14 @@
+const Car=({id,mark,model,type,releaseYear,regExpireDate})=>{
+    return(
+        <div>
+            <p>{id}</p>
+            <p>{mark}</p>
+            <p>{model}</p>
+            <p>{type}</p>
+            <p>{releaseYear}</p>
+            <p>{regExpireDate}</p>
+        </div>
+    )
+}
+
+export default Car;
---
+++ CarDealerShip/src/data.js
@@ -0,0 +1,38 @@
+import { v4 as uuid } from 'uuid';
+
+const Cars=[
+    {
+    id:uuid(),
+    mark:"Toyota",
+    model:"Corolla",
+    type:"Sedan",
+    releaseYear:2021,
+    regExpireDate:"2022-12-31"
+    },
+    {
+    id:uuid(),
+    mark:"Honda",
+    model:"Civic",
+    type:"Sedan",
+    releaseYear:2022,
+    regExpireDate:"2023-12-31"
+    },
+    {
+    id:uuid(),
+    mark:"Suzuki",
+    model:"Swift",
+    type:"Hatchback",
+    releaseYear:2021,
+    regExpireDate:"2022-12-31"
+    },
+    {
+    id:uuid(),
+    mark:'Toyota',
+    model:'Yaris',
+    type:'SUV',
+    releaseYear:2021,
+    regExpireDate:'2022-12-31'
+    }
+];
+
+export default Cars;
